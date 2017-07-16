75f5dde23ed96ab35386e234cdb53ed8e765f9d8
biswarup <bis.banerjee.bb@gmail.com>
2017-07-16 21:18:51 +0530

    applied templete driven form using ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { NewCompComponent } from './new-comp/new-comp.component';
@@ -12,7 +13,8 @@ import { SortPipe } from './app.sort'
     NewCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
