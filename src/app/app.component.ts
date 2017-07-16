7fabd2d8ea7da71298a8eb275cf6206a0ab47f8f
biswarup <bis.banerjee.bb@gmail.com>
2017-07-16 20:03:08 +0530

    added bootstrap elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  arr  = [4,2,7,3,1,3,5,1,9];
+  
 }
